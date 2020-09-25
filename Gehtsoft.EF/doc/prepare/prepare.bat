e2b204438ea9848a7bcfbc952cdbaf511d7b6305
Nikolay Gekht <nikolay.gekht@gehtsoftusa.com>
2020-09-25 16:49:54 -0400

    - update docprepare to use msbuild approach

--- Gehtsoft.EF/doc/prepare/prepare.bat
+++ Gehtsoft.EF/doc/prepare/prepare.bat
@@ -1,8 +1,4 @@
 @echo off
-if exist dst del dst\*.* /q /s > nul
-if not exist dst md dst
-if exist src del src\*.* /q /s > nul
-if not exist src md src
-"%sandcastle%\ProductionTools\MrefBuilder.exe" ..\..\Gehtsoft.Ef.Db.SqlDb\bin\Release\net461\Gehtsoft.Ef.Entities.dll ..\..\Gehtsoft.Ef.Db.SqlDb\bin\Release\net461\Gehtsoft.Ef.Db.Sqldb.dll ..\..\Gehtsoft.Ef.FTS\bin\Release\net461\Gehtsoft.Ef.FTS.dll ..\..\Gehtsoft.EF.Db.SqlDb.OData\bin\Release\net461\Gehtsoft.EF.Db.SqlDb.OData.dll  /out:doc-source.xml
 if not exist src mkdir src
-%docgen%\bin\docgen prepare.xml
+if not exist src\null.ds copy /b nul src\null.ds
+dotnet build project.proj /t:Scan,Raw
