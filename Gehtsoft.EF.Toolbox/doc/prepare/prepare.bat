97371392618348d4e2566c0be2c3f2f9f5c652af
Nikolay Gekht <nikolay.gekht@gehtsoftusa.com>
2021-02-13 16:13:21 -0500

    - update documentation scripts

--- Gehtsoft.EF.Toolbox/doc/prepare/prepare.bat
+++ Gehtsoft.EF.Toolbox/doc/prepare/prepare.bat
@@ -1,8 +1,4 @@
 @echo off
-if exist dst del dst\*.* /q /s > nul
-if not exist dst md dst
-if exist src del src\*.* /q /s > nul
-if not exist src md src
-"%sandcastle%\ProductionTools\MrefBuilder.exe" ..\..\Gehtsoft.EF.Mapper\bin\Debug\net461\Gehtsoft.Mapper.dll ..\..\Gehtsoft.EF.Mapper\bin\Release\net461\Gehtsoft.EF.Mapper.dll ..\..\Gehtsoft.Validator\bin\Release\net461\Gehtsoft.Validator.dll  /out:doc-source.xml
 if not exist src mkdir src
-%docgen%\bin\docgen prepare.xml
+if not exist src\null.ds copy /b nul src\null.ds
+dotnet build project.proj /t:Scan,Raw
