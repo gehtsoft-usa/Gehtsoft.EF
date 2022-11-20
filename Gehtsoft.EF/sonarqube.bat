fd2e7a629e0a7e67e226ee767d4613fd04453feb
Nikolay Gekht <nikolay.gekht@gehtsoftusa.com>
2022-11-20 12:50:20 -0500

    - remove sonarqube

--- Gehtsoft.EF/sonarqube.bat
+++
@@ -1,3 +0,0 @@
-dotnet sonarscanner begin /k:"Gehtsoft.EF" /d:sonar.host.url="http://localhost:9000"  /d:sonar.login="233a442e3701582a3e1b786872080484cb995ff0"
-dotnet build Gehtsoft.EF.sln
-dotnet sonarscanner end /d:sonar.login="233a442e3701582a3e1b786872080484cb995ff0"
